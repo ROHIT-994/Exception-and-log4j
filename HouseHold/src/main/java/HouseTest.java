7e8f9f67c6745c0a56f7e0f46cbecef4a88b5075
ROHIT-994 <pasumarthirohith@gmail.com>
2020-02-26 14:27:08 +0530

    log4j for house holds

---
+++ HouseHold/src/main/java/HouseTest.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+
+
+public class HouseTest {
+	House h;
+	@Before
+	public void init()
+	{
+		h=new House();
+	}
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		assertEquals(4500000,h.FullyHomeCost("HighStandardMaterials",2500),0);
+		
+	}
+
+}
